a0ec51b107b720c8c8f4dfc0c14c11f5e1f162cd
LILITHl <Saosao007888@gmail.com>
2022-03-25 19:17:03 +0800

    chenge to while i > 0

--- password1.py
+++ password1.py
@@ -1,6 +1,13 @@
+# 碼重試程式
+# password = 'a123456'
+# 讓使用者重複輸入密碼
+# 最多輸入3次
+# 如果正確 就印出 "登入成功!"
+# 如果不正確 就印出 "密碼錯誤! 還有__次機會!"
+
 password = 'a123456'
 i = 3 #剩餘機會
-while True :
+while i > 0:
 	pwd = input('請輸入密碼: ')	
 	if pwd == password:
 		print('登入成功')
@@ -9,5 +16,4 @@ while True :
 	else:
 		i = i - 1
 		print('密碼錯誤!還有', i,'次機會')	
-		if i == 0:
-			break
+	
